7496492133dda7ae80b8f320476795804f7ee222
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-06-06 01:01:47 +0100

    added a function to return dict

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,28 @@
 #!/usr/bin/python3
 """Base class for all subsequent classes"""
+from datetime import datetime
+from uuid import uuid4
+import json
 
 
 class BaseModel:
     """The Base class"""
-    pass
+    
+    def __init__(self):
+        """initialise the base class"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now().isoformat()
+        self.updated_at = datetime.now().isoformat()
+
+    def __str__(self):
+        """print the class info"""
+        print(type(self.__dict__))
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    
+    def to_dict(self):
+        """deserialize class object"""
+        return self.__dict__
+
+    def save(self):
+        """save the changes made to an object"""
+        self.updated_at = datetime.now().isoformat()
