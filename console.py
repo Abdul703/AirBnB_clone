49249fee8ec77a237d34b8a946757039e7f144ba
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-06-09 21:11:08 +0100

    show method to print an object based on class name and Id added

--- console.py
+++ console.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Command Prompt Module"""
 import cmd
+from models.base_model import BaseModel
+from models import storage
+
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -23,6 +26,46 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing on empty input line."""
         pass
+
+    def do_create(self, arg):
+        """Create a BaseModel object and save it in file.json."""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        if arg == 'BaseModel':
+            obj = BaseModel()
+            obj.save()
+            print(obj.id)
+        else:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """Show the string representation of an instance based on the class name and id."""
+        args = arg.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        
+        class_name = args[0]
+        if class_name != 'BaseModel':
+            print("** class doesn't exist **")
+            return
+
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        obj_id = args[1]
+
+        all_objects = storage.all()
+        key = f'{class_name}.{obj_id}'
+
+        try:
+            obj = all_objects[key]
+            print(obj)
+        except KeyError:
+            print("** no instance found **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
