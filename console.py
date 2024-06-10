2cfeedb1822e1c66709c1abc08036f853c1afb57
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-06-10 10:16:50 +0100

    implemented update method

--- console.py
+++ console.py
@@ -138,6 +138,53 @@ class HBNBCommand(cmd.Cmd):
             
             if not found:
                 print("** class doesn't exist **")
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id by
+           adding or updating attribute
+
+        Syntax:
+            update <class name> <id> <attribute name> "<attribute value>"
+        """
+
+        args = arg.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        if class_name != 'BaseModel':
+            print("** class doesn't exist **")
+            return
+
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        obj_id = args[1]
+        
+        all_objects = storage.all()
+        key = f'{class_name}.{obj_id}'
+
+        try:
+            obj = all_objects[key]
+            if len(args) == 2:
+                print("** attribute name missing **")
+                return
+            if len(args) == 3:
+                print("** value missing **")
+                return
+
+            attr_name, attr_value = args[2], args[3].strip('"')
+            if hasattr(obj, attr_name):
+                attr_type = type(getattr(obj, attr_name))
+                setattr(obj, attr_name, attr_type(attr_value))
+            else:
+                setattr(obj, attr_name, attr_value)
+            obj.save()
+
+        except KeyError:
+            print("** no instance found **")
           
 
 if __name__ == '__main__':
