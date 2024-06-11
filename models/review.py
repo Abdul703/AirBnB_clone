6d61c3df05fcdcdb05965606781344eb7ce161e3
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-06-11 18:20:44 +0100

    review class added

--- models/review.py
+++ models/review.py
@@ -4,9 +4,15 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Review class
-
-    Args:
-        BaseModel (class): Base class
     """
-    pass
+    Review class that inherits from BaseModel.
+
+    Attributes:
+        place_id (str): The id of the place reviewed.
+        user_id (str): The user's id that reviewed.
+        text (str): User's comment.
+    """
+
+    place_id = ''
+    user_id = ''
+    text = ''
