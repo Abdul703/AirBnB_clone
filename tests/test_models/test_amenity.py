1fa8261abbdee6ca45c3997a6b0d4e49ffbcb91a
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-06-11 18:55:43 +0100

    test for amenity model added

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
-"""Testing The Amenity class"""
+"""Unit tests for the Amenity class."""
 import unittest
+from models.base_model import BaseModel
 from models.amenity import Amenity
 
 
 class TestAmenity(unittest.TestCase):
     """Test cases for the Amenity class."""
-    pass
+
+    def test_class_attributes(self):
+        """Test that Amenity class attributes exist and are of correct type."""
+        self.assertTrue(hasattr(Amenity, 'name'))
+        self.assertIsInstance(Amenity.name, str)
+
+    def test_instance_attributes(self):
+        """Test that Amenity instance attributes are initialized correctly."""
+        amenity = Amenity()
+        self.assertEqual(amenity.name, '')
+
+    def test_inheritance(self):
+        """Test that Amenity class inherits from BaseModel."""
+        amenity = Amenity()
+        self.assertIsInstance(amenity, BaseModel)
+
+
+if __name__ == '__main__':
+    unittest.main()
