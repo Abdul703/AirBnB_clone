1761892645342416529402ccc1e0bf7f5248feb3
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-06-11 18:13:49 +0100

    state class added

--- models/state.py
+++ models/state.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""State class"""
+"""State class model"""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """State class
-
-    Args:
-        BaseModel (class): Base class
     """
-    pass
+    User class that inherits from BaseModel.
+
+    Attributes:
+        name (str): The name of the state.
+    """
+
+    name = ''
