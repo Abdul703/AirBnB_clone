55c2f2a50d893ee9e69a2ad990feb5168e86910c
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-06-11 18:40:49 +0100

    test for state model added

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python3
-"""Testing The State class"""
+"""Unit tests for the State class."""
 import unittest
+from models.base_model import BaseModel
 from models.state import State
 
 
 class TestState(unittest.TestCase):
     """Test cases for the State class."""
-    pass
+
+    def test_class_attributes(self):
+        """Test that State class attribute exist and is of correct type."""
+        self.assertTrue(hasattr(State, 'name'))
+        self.assertIsInstance(State.name, str)
+        
+
+    def test_instance_attributes(self):
+        """Test that State instance attribute is initialized correctly."""
+        state = State()
+        self.assertEqual(state.name, '')
+
+    def test_inheritance(self):
+        """Test that State class inherits from BaseModel."""
+        state = State()
+        self.assertIsInstance(state, BaseModel)
+
+
+if __name__ == '__main__':
+    unittest.main()
